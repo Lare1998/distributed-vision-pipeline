92146acd3aeb9a51d4b1354d7390972f81b72191
Alexander Reed <zetwitatelster@gmx.net>
2023-01-02 14:07:51 -0500

    refactor: Improve LLM agent communication protocol

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from distributed_vision.pipeline import DistributedVisionPipeline
+import os
+import cv2
+import numpy as np
+
+if __name__ == "__main__":
+    print("Starting Distributed Vision Pipeline application...")
+
+    # Create a dummy video file for testing
+    dummy_video_path = "dummy_video.mp4"
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(dummy_video_path, fourcc, 20.0, (640, 480))
+    for _ in range(100): # 100 frames
+        frame = np.random.randint(0, 255, (480, 640, 3), dtype=np.uint8)
+        out.write(frame)
+    out.release()
+
+    pipeline = DistributedVisionPipeline(dummy_video_path, num_workers=4)
+    results = pipeline.run()
+    print(f"Total frames processed: {len(results)}")
+    
+    # Clean up dummy video
+    os.remove(dummy_video_path)
+
+    print("Distributed Vision Pipeline application finished.")
+
+# Update on 2023-01-02 00:00:00
---
+++ src/distributed_vision/pipeline.py
@@ -0,0 +1,102 @@
+
+import cv2
+import numpy as np
+import time
+from typing import List, Dict, Any
+import multiprocessing
+
+class DataLoader:
+    """Simulates loading video frames from a source."""
+    def __init__(self, video_path: str, batch_size: int = 32):
+        self.video_path = video_path
+        self.batch_size = batch_size
+        self.cap = cv2.VideoCapture(video_path)
+        if not self.cap.isOpened():
+            raise IOError(f"Cannot open video source {video_path}")
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> List[np.ndarray]:
+        frames = []
+        for _ in range(self.batch_size):
+            ret, frame = self.cap.read()
+            if not ret:
+                self.cap.release()
+                raise StopIteration
+            frames.append(frame)
+        return frames
+
+    def release(self):
+        self.cap.release()
+
+class FrameProcessor:
+    """Processes individual video frames (e.g., object detection, feature extraction)."""
+    def __init__(self, processor_id: int):
+        self.processor_id = processor_id
+
+    def process_frame(self, frame: np.ndarray) -> Dict[str, Any]:
+        # Simulate a complex processing task
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Simulate object detection: find contours
+        contours, _ = cv2.findContours(gray_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        num_objects = len(contours)
+        
+        # Simulate some feature extraction
+        mean_pixel_value = np.mean(gray_frame)
+        
+        time.sleep(0.01) # Simulate processing time
+        return {
+            "processor_id": self.processor_id,
+            "timestamp": time.time(),
+            "num_objects": num_objects,
+            "mean_pixel_value": mean_pixel_value,
+            "frame_shape": frame.shape
+        }
+
+def process_batch(frames: List[np.ndarray], processor_id: int) -> List[Dict[str, Any]]:
+    processor = FrameProcessor(processor_id)
+    results = [processor.process_frame(frame) for frame in frames]
+    return results
+
+class DistributedVisionPipeline:
+    """Orchestrates distributed processing of video frames."""
+    def __init__(self, video_path: str, num_workers: int = multiprocessing.cpu_count()):
+        self.video_path = video_path
+        self.num_workers = num_workers
+        self.data_loader = DataLoader(video_path)
+        self.pool = multiprocessing.Pool(processes=self.num_workers)
+
+    def run(self):
+        print(f"Starting distributed vision pipeline with {self.num_workers} workers...")
+        all_results = []
+        try:
+            for i, batch in enumerate(self.data_loader):
+                print(f"Processing batch {i+1}...")
+                # Distribute batch processing to workers
+                batch_results = self.pool.apply_async(process_batch, (batch, i % self.num_workers))
+                all_results.extend(batch_results.get())
+        except StopIteration:
+            print("End of video stream.")
+        finally:
+            self.data_loader.release()
+            self.pool.close()
+            self.pool.join()
+        print("Distributed vision pipeline finished.")
+        return all_results
+
+if __name__ == "__main__":
+    # Example Usage: Create a dummy video file for testing
+    dummy_video_path = "dummy_video.mp4"
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(dummy_video_path, fourcc, 20.0, (640, 480))
+    for _ in range(100): # 100 frames
+        frame = np.random.randint(0, 255, (480, 640, 3), dtype=np.uint8)
+        out.write(frame)
+    out.release()
+
+    pipeline = DistributedVisionPipeline(dummy_video_path, num_workers=4)
+    results = pipeline.run()
+    print(f"Total frames processed: {len(results)}")
+    # Clean up dummy video
+    os.remove(dummy_video_path)
